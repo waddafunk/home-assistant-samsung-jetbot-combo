b6c212757c6b79dcface007856f7fa899d5f27ee
waddafunk <jacopo.piccirillo@gmail.com>
2025-07-20 20:41:35 +0200

    fix app credentials, bump to 2-0.2

--- custom_components/samsung_jetbot_combo/application_credentials.py
+++ custom_components/samsung_jetbot_combo/application_credentials.py
@@ -5,18 +5,13 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-# SmartThings OAuth 2.0 endpoints
-AUTHORIZATION_SERVER = AuthorizationServer(
-    authorize_url="https://account.smartthings.com/oauth/authorize",
-    token_url="https://account.smartthings.com/oauth/token",
-)
 
-
-async def async_get_auth_implementation(
-    hass: HomeAssistant, auth_domain: str, credential
-) -> AuthorizationServer:
-    """Return auth implementation for domain."""
-    return AUTHORIZATION_SERVER
+async def async_get_authorization_server(hass: HomeAssistant) -> AuthorizationServer:
+    """Return authorization server for SmartThings OAuth."""
+    return AuthorizationServer(
+        authorize_url="https://account.smartthings.com/oauth/authorize",
+        token_url="https://account.smartthings.com/oauth/token",
+    )
 
 
 async def async_get_description_placeholders(hass: HomeAssistant) -> dict[str, str]:
